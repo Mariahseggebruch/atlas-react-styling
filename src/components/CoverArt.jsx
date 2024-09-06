50cbd1f50d620d912c3af660a0ba754349da2c89
Mariahseggebruch <mariahseggebruch@gmail.com>
2024-09-06 16:48:51 -0500

    pushing current work, had my components working and now they're not

--- src/components/CoverArt.jsx
+++ src/components/CoverArt.jsx
@@ -3,8 +3,7 @@ import coverArtImage from '../assets/placeholder.svg';
 
 const CoverArt = () => {
   return (
-    <div className="h-full w-full bg-no-repeat bg-center bg-cover" style={{ backgroundImage: `url(${coverArtImage})` }}>
-      <img src={coverArtImage} alt="Cover Art" className="absolute top-0 left-0" />
+    <div className="h-40 w-40 bg-no-repeat bg-center bg-cover" style={{ backgroundImage: `url(${coverArtImage})` }}>
     </div>
   );
 };
---
+++ src/components/SongTitle.jsx
@@ -0,0 +1,9 @@
+export default function SongTitle() {
+    return (
+      <div className="text-align-left">
+        <h1 className="text-3xl font-bold">Tidal Drift</h1>
+        <p className="text-lg">Echoes of the Sea</p>
+      </div>
+    );
+};
+
